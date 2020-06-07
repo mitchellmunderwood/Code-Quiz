712683711c76aa057b77c82d3ae6347772a0e264
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-06-07 16:54:57 -0400

    Got Basic Highscore and Timer functionality added

--- script.js
+++ script.js
@@ -44,33 +44,52 @@ var answer_b = document.getElementById("answer_b");
 var answer_c = document.getElementById("answer_c");
 var answer_d = document.getElementById("answer_d");
 var complete_card = document.getElementById("complete_card");
+var response = document.getElementById("response");
+var timerEl = document.getElementById("timer");
 
 var index = 1;
-
+var score = 0;
+var timer = 60;
 var question_box = document.querySelector(".question_box");
 
 question_box.addEventListener("click", function(event) {
     event.preventDefault();
     var answer_selected = '';
-    
     var correct_answer = questions["q"+index].answer;
-    console.log(correct_answer);
     if (event.target.matches("button")) {
         answer_selected = event.target.getAttribute("data");
     }
-    console.log("user selected", answer_selected);
+    index++;
     if (answer_selected === correct_answer) {
-        index++;
-        
-    };
+        score += 25; 
+        renderResponse("Correct!");
+       
+    } else {
+        renderResponse("Incorrect!");
+        timer -= 5;
+    }
     if (index > 4) {
+        
         renderComplete();
     } else {
         renderQuestion(index);
     }
+    
 })
 
+function renderResponse(res) {
+    if (res === "Correct!") {
+        response.style.color = "green";
+    } else {
+        response.style.color = "red";
+    }
+    response.textContent = res;
+    setTimeout(function() {response.textContent = "";}, 500);
+}
+
+
 function renderQuestion(num) {
+    // response.textContent = "";
     var current_question = questions["q"+num]
     question_text.textContent = current_question.question;
     answer_a.textContent = current_question.choices.a;
@@ -82,16 +101,103 @@ function renderQuestion(num) {
 function renderComplete() {
     question_card.style.display = "none";
     complete_card.style.display = "block";
+    var scoreEl = document.getElementById("score");
+    scoreEl.textContent = score;
 }
 
 
 var start_button = document.getElementById("start_button");
-var grade = 100;
 
 start_button.addEventListener("click", function() {
+    
+    var time = setInterval(function() {
+        timerEl.textContent = timer;
+        timer -= 1;
+        if (timer < 0 || index > 4) {
+            clearInterval(time);
+            timerEl.textContent = 0;
+            renderComplete();
+            
+        }
+    }, 1000);
     start_card.style.display = "none";
     question_card.style.display = "block";
     renderQuestion(1);
+
+})
+
+var submit_btn = document.getElementsByClassName("submit_btn")[0];
+var initials = document.getElementById("initials");
+
+submit_btn.addEventListener("click", function(event) {
+    event.preventDefault();
+    var player_name = initials.value;
+    initials.value = "";
+    player_score = score;
+    submitHighscore(player_name, player_score);
+    renderHighscores();
+})
+
+function submitHighscore(name, score) {
+    var entry = [name, score];
+    highscores.push(entry);
+}
+
+var back_btn = document.getElementById("back_btn"); 
+var clear_btn = document.getElementById("clear_btn"); 
+var header = document.getElementById("header");
+var main = document.getElementById("main");
+var highscoresEl = document.getElementById("highscores");
+var highscores = [["MU",100],["AGU", 100]];
+var score_list = document.getElementById("score_list");
+
+function renderHighscores() {
+    header.style.display = "none";
+    main.style.display = "none";
+    highscoresEl.style.display = "flex";
+    // clear out existing children of highscore
+    score_list.innerHTML = "";
+
+    for (i=0; i<highscores.length; i++) {
+        var entry = document.createElement("div");
+        entry.textContent = ((i+1)+". "+highscores[i][0]+" - "+highscores[i][1]);
+        score_list.appendChild(entry);
+        
+    }
+}
+
+function clearHighscores() {
+    highscores = [];
+    renderHighscores()
+}
+
+function startOver() {
+    header.style.display = "block";
+    main.style.display = "block";
+    start_card.style.display = "block";
+    highscoresEl.style.display = "none";
+    complete_card.style.display = "none";
+    timer = 60;
+    index = 1;
+    score = 0;
+}
+
+back_btn.addEventListener("click", function() {
+    event.preventDefault();
+    startOver();
+})
+
+clear_btn.addEventListener("click", function(event) {
+    event.preventDefault();
+    clearHighscores();
 })
 
 
+// make sure "view highscores works"
+// fix css on submit page
+// refactor code for more intuitive feel
+
+var nav_link_scores = document.getElementById("high_scores");
+nav_link_scores.addEventListener("click", function() {
+    renderHighscores();
+})
